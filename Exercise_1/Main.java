cf141dc000e89ee89795bcd72488514c010a970a
Sergey Konyakhin <sergekonyakhin@gmail.com>
2023-03-23 18:51:49 +0300

    Exercise_1 modified

--- Exercise_1/Main.java
+++ Exercise_1/Main.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        
-        System.out.print("Enter number n: ");
-        int n = scanner.nextInt();
-        
-        
-        int triangleNumber = 0;
-        for (int i = 1; i <= n; i++) {
-            triangleNumber += i;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter number n: ");
+            int n = scanner.nextInt();
+            
+            
+            int triangleNumber = 0;
+            for (int i = 1; i <= n; i++) {
+                triangleNumber += i;
+            }
+            System.out.println("n-th triangular number: " + triangleNumber);
+            
+            
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("n! = " + factorial);
         }
-        System.out.println("n-th triangular number: " + triangleNumber);
-        
-        
-        int factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        System.out.println("n! = " + factorial);
     }
 }
